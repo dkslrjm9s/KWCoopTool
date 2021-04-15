f9028de3216e474ea4a432818d909d3af121a26d
Tuğçe Küçükoğlu <tugcekucukoglu.860@gmail.com>
2021-04-15 11:23:56 +0300

    refactor splitterpanel

--- src/components/PanelsDemo.vue
+++ src/components/PanelsDemo.vue
@@ -179,36 +179,34 @@
 		<div class="p-col-12">
 			<div class="card">
 				<h5>Splitter</h5>
-				<div class="p-grid">
-					<Splitter style="height: 300px" class="p-mb-5">
-						<SplitterPanel :size="40" :minSize="10" style="overflow: scroll">
-							<p class="p-col p-m-3">
-								Lorem, ipsum dolor sit amet consectetur adipisicing elit. Consequatur pariatur recusandae rerum atque nisi ipsum fuga numquam 
-								distinctio obcaecati quibusdam repellat, est assumenda quam perferendis reprehenderit, blanditiis, excepturi facilis! Voluptatem.
-							</p>
-						</SplitterPanel>
-						<SplitterPanel :size="60" style="overflow: scroll">
-							<Splitter layout="vertical">
-								<SplitterPanel :size="15" style="overflow: scroll">
-									<p class="p-col p-m-3">
-										At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti 
-										quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, 
-										id est laborum et dolorum fuga. Et harum quidem rerum facilis est et expedita distinctio. Nam libero tempore, cum soluta nobis 
-										est eligendi optio cumque nihil impedit quo minus.
-									</p>
-								</SplitterPanel>
-								<SplitterPanel :size="85" style="overflow: scroll">
-									<p class="p-col p-m-3">
-										Temporibus autem quibusdam et aut officiis debitis aut rerum necessitatibus saepe eveniet ut et voluptates repudiandae sint et 
-										molestiae non recusandae. Itaque earum rerum hic tenetur a sapiente delectus, ut aut reiciendis voluptatibus maiores alias consequatur 
-										aut perferendis doloribus asperiores repellat. Donec vel volutpat ipsum. Integer nunc magna, posuere ut tincidunt eget, 
-										egestas vitae sapien. Morbi dapibus luctus odio.
-									</p>
-								</SplitterPanel>
-							</Splitter>
-						</SplitterPanel>
-					</Splitter>
-				</div>
+				<Splitter style="height: 300px" class="p-mb-5">
+					<SplitterPanel :size="40" :minSize="10" style="overflow: scroll">
+						<p class="p-col">
+							Lorem, ipsum dolor sit amet consectetur adipisicing elit. Consequatur pariatur recusandae rerum atque nisi ipsum fuga numquam 
+							distinctio obcaecati quibusdam repellat, est assumenda quam perferendis reprehenderit, blanditiis, excepturi facilis! Voluptatem.
+						</p>
+					</SplitterPanel>
+					<SplitterPanel :size="60" style="overflow: scroll">
+						<Splitter layout="vertical">
+							<SplitterPanel :size="25" style="overflow: scroll">
+								<p class="p-col">
+									At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti 
+									quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, 
+									id est laborum et dolorum fuga. Et harum quidem rerum facilis est et expedita distinctio. Nam libero tempore, cum soluta nobis 
+									est eligendi optio cumque nihil impedit quo minus.
+								</p>
+							</SplitterPanel>
+							<SplitterPanel :size="75" style="overflow: scroll">
+								<p class="p-col">
+									Temporibus autem quibusdam et aut officiis debitis aut rerum necessitatibus saepe eveniet ut et voluptates repudiandae sint et 
+									molestiae non recusandae. Itaque earum rerum hic tenetur a sapiente delectus, ut aut reiciendis voluptatibus maiores alias consequatur 
+									aut perferendis doloribus asperiores repellat. Donec vel volutpat ipsum. Integer nunc magna, posuere ut tincidunt eget, 
+									egestas vitae sapien. Morbi dapibus luctus odio.
+								</p>
+							</SplitterPanel>
+						</Splitter>
+					</SplitterPanel>
+				</Splitter>
 			</div>
 		</div>
 	</div>
