5dfc89099a875cfc4f03ddebe8380be74a0167c7
Tuğçe Küçükoğlu <tugcekucukoglu.860@gmail.com>
2021-08-16 16:56:42 +0300

    refactor

--- src/components/Documentation.vue
+++ src/components/Documentation.vue
@@ -104,6 +104,7 @@ data() {
                     {label: "Float Label", icon: "pi pi-bookmark", to: "/floatlabel"},
                     {label: "Invalid State", icon: "pi pi-exclamation-circle", to: "/invalidstate"},
                     {label: "Button", icon: "pi pi-mobile", to: "/button", class: 'rotated-icon'},
+                    {label: "Speed Dial", icon: "pi pi-tablet", to: "/speeddial"},
                     {label: "Table", icon: "pi pi-table", to: "/table"},
                     {label: "List", icon: "pi pi-list", to: "/list"},
                     {label: "Tree", icon: "pi pi-share-alt", to: "/tree"},
