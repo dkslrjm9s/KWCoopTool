0b78255693abab13ae6195ce3a03f3128dd86a7c
Tuğçe Küçükoğlu <tugcekucukoglu.860@gmail.com>
2021-08-16 14:34:38 +0300

    fix login

--- src/pages/Login.vue
+++ src/pages/Login.vue
@@ -3,20 +3,20 @@
 		<div class="login-wrapper">
 			<div class="login-panel">
 				<a @click="goDashboard" class="logo">
-                <img src="assets/layout/images/logo-freya-single.svg" alt="freya-layout" />
-            </a>
-            <InputText id="email" placeholder="Email" />
-            <Password id="password" placeholder="Password" :feedback="false" />
-            <Button label="LOGIN" type="button" ></button>
-            <a href="#">forget password?</a>
-            <p>Don’t you have an account, <a href="#">sign up</a></p>
+                    <img src="assets/layout/images/logo-freya-single.svg" alt="freya-layout" />
+                </a>
+                <InputText id="email" placeholder="Email" />
+                <Password id="password" placeholder="Password" :feedback="false" />
+                <Button label="LOGIN" type="button" ></button>
+                <a href="#">forget password?</a>
+                <p>Don’t you have an account, <a href="#">sign up</a></p>
+            </div>
         </div>
         <div class="login-footer">
 			<h4>freya</h4>
             <h6>Copyright Ⓒ PrimeTek Informatics</h6>
         </div>
     </div>
-</div>
 </template>
 
 <script>
